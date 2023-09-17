540b39c7fad70665bfb9791aa78712803dfcae13
Jose Mori <joosem@gmail.com>
2023-09-16 22:36:47 -0500

    AP-4 add profiles and create the docker image to have the db

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+/*INSERT INTO play_schema.play (id, user_id, bet_id, choice_id, amount, registration_date) VALUES(1, 1, 1, 1, 10.4, NOW());
+INSERT INTO play_schema.play (id, user_id, bet_id, choice_id, amount, registration_date) VALUES(2, 1, 1, 2, 20.4, NOW());
+INSERT INTO play_schema.play (id, user_id, bet_id, choice_id, amount, registration_date) VALUES(3, 1, 2, 5, 30.4, NOW());*/
