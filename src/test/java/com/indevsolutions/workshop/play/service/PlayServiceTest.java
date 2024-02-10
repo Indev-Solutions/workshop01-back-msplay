43c4776523a06d30540c89c72bea626fe4a24208
josem <josem@host.docker.internal>
2024-02-10 11:28:19 -0500

    AP-4 fix unit test and update the sonar configuration to exclude some directories

--- src/test/java/com/indevsolutions/workshop/play/service/PlayServiceTest.java
+++ src/test/java/com/indevsolutions/workshop/play/service/PlayServiceTest.java
@@ -28,16 +28,18 @@ import com.indevsolutions.workshop.play.dto.shared.BetDTO;
 import com.indevsolutions.workshop.play.dto.shared.BetOptionDTO;
 import com.indevsolutions.workshop.play.repository.PlayRepository;
 
-
 @ExtendWith(MockitoExtension.class)
 class PlayServiceTest {
-	
+
 	@Mock
 	public PlayRepository playRepository;
 
 	@Mock
 	public BetService betService;
-	
+
+	@Mock
+	public MessageService messageService;
+
 	@InjectMocks
 	public PlayService playService;
 
@@ -46,52 +48,59 @@ class PlayServiceTest {
 		when(betService.findBetsByIds(anySet())).thenReturn(bet());
 		ReflectionTestUtils.setField(playService, "modelMapper", new ModelMapper());
 	}
-	
+
 	@Test
 	void testFindLatesPlays() {
 		when(playRepository.findTop5ByUserIdOrderByRegistrationDateDesc(anyLong())).thenReturn(plays(1l));
 
 		var plays = playService.findLatestPlays(1l);
-		
+
 		assertNotNull(plays);
 		assertEquals(1, plays.size());
 	}
-	
+
 	@Test
 	void testFindLatesPlaysWihtoutResult() {
 		when(playRepository.findTop5ByUserIdOrderByRegistrationDateDesc(anyLong())).thenReturn(List.of());
 
 		var plays = playService.findLatestPlays(1l);
-		
+
 		assertNotNull(plays);
 		assertEquals(0, plays.size());
 	}
-	
+
 	@Test
 	void testCreatePlay() {
-		var play = play(1l,1l,1l);
-		
+		var play = play(1l, 1l, 1l);
+
 		when(playRepository.save(any(Play.class))).thenReturn(play);
-		
+
 		var result = playService.createPlay(play);
-		
+
 		assertNotNull(result);
 	}
-	
+
 	@Test
 	void testCreatePlayWithWrongChoide() {
-		var play = play(1l,1l,2l);
-		
-	    Exception exception = assertThrows(ResponseStatusException.class, () -> {
+		when(messageService.getMessage(any(Error.class))).thenAnswer(a-> {			
+			if(a.getArgument(0)== Error.CHOICE_NOT_VALID) {
+				return "The choiceId is not valid.";
+			}
+			return "Error";
+		});
+
+		var play = play(1l, 1l, 2l);
+
+		Exception exception = assertThrows(ResponseStatusException.class, () -> {
 			playService.createPlay(play);
-	    });
+		});
 
-	    String expectedMessage = "The choiceId is not valid.";
-	    String actualMessage = exception.getMessage();
+		String expectedMessage = "The choiceId is not valid.";
+		String actualMessage = exception.getMessage();
 
-	    assertTrue(actualMessage.contains(expectedMessage));
+		assertTrue(actualMessage.contains(expectedMessage));
 	}
-	
+
 	private List<BetDTO> bet() {
 		var newBet = new BetDTO();
 
@@ -99,26 +108,26 @@ class PlayServiceTest {
 		newBet.setMatchDate(tomorrow);
 		newBet.setLeagueId(1l);
 		newBet.setId(1l);
-		
+
 		var option = new BetOptionDTO();
 		option.setId(1l);
 		newBet.setOptions(Set.of(option));
-		
+
 		return List.of(newBet);
 	}
-	
+
 	private List<Play> plays(Long userId) {
 		var play = play(userId, 1l, 1l);
 		return List.of(play);
 	}
-	
+
 	private Play play(Long userId, Long betId, Long choiceId) {
 		var play = new Play();
 		play.setId(1l);
 		play.setBetId(betId);
 		play.setChoiceId(choiceId);
 		play.setUserId(userId);
-		
+
 		return play;
 	}
 
